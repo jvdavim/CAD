1eeb77f0f3b88a4495fd05968bc67870d32cfc47
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2018-04-09 11:28:51 -0300

    Fix index error
    Fix index error.

--- t1/mult.f
+++ t1/mult.f
@@ -1,20 +1,20 @@
       program mult
-              integer, parameter::N=5
+              integer, parameter::N=4000
               integer::i, j
               double precision, dimension(:,:), allocatable::mat
               double precision, dimension(:), allocatable::vec, res
               real::start, finish, s
               allocate(mat(N+1,N+1),vec(N+1),res(N+1))
-              do i=0,N
-                 do j=0,N
+              do i=1,N
+                 do j=1,N
                     mat(i,j)=rand(0)*1000
                  end do
                  vec(i)=rand(0)*1000
               end do
               call cpu_time(start)
-              do i=0,N
+              do i=1,N
                  s=0
-                 do j=0,N
+                 do j=1,N
                     s=s+mat(i,j)*vec(j)
                  end do
                  res(i)=s
